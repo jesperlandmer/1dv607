a5a8d36df1a91491105bbf81751cffe4a5a5fca0
[Jesper] <[jp222vq@student.lnu.se]>
2017-10-21 09:58:35 -0700

    Initial fork

---
+++ test/Views/ConsoleView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Views
+{
+    public class ConsoleView
+    {
+        public ConsoleView()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
